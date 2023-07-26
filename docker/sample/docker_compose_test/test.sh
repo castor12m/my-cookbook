bbad501540fae7eb7a17e01a8390bbd02d50cb35
castor_mac <castor@naraspace.com>
2023-07-26 15:16:31 +0900

    docker sample 수정

--- docker/sample/docker_compose_test/test.sh
+++
@@ -1,7 +0,0 @@
-for var in {1..1000..1} 
-do 
-    echo `uptime` >> result.txt
-    echo `ls /home | grep test` >> result.txt
-    sleep 1
-    #
-done
--- docker/sample/docker_compose_test/test_b/test.sh
+++ docker/sample/docker_compose_test/test_b/test.sh
@@ -1,7 +1,8 @@
+
 for var in {1..1000..1} 
 do 
     echo `uptime` >> result.txt
-    echo `ls /home | grep test` >> result.txt
+    echo `ls /home/data | grep test` >> result.txt
     sleep 1
     #
 done
